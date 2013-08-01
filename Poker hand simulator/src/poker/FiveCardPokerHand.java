bbfd623e404625ae6a1ec7ad96daecf139e0d2da
Sebastian Björkqvist <sebastian.bjorkqvist@helsinki.fi>
2013-08-01 21:11:20 +0300

    Wrote some javadocs.

--- Poker hand simulator/src/poker/FiveCardPokerHand.java
+++ Poker hand simulator/src/poker/FiveCardPokerHand.java
@@ -6,17 +6,33 @@ import java.util.List;
 
 /**
  * Represents a standard five card poker hand with cards from a 52 card deck.
- * 
+ *
  * @author Sebastian Björkqvist
  */
 public class FiveCardPokerHand {
 
     private List<Card> cards;
 
+    /**
+     * Creates a new empty five card hand.
+     */
     public FiveCardPokerHand() {
         cards = new ArrayList<Card>();
     }
-    
+
+    /**
+     * Adds a card to the hand.
+     *
+     * The hand doesn't accept any new cards if it is full, i.e. if it already
+     * contains five cards.
+     *
+     * Null-cards can't be added to a hand.
+     *
+     * @param card A Card-object
+     * @return true if the card was added, false if it wasn't.
+     * @throws IllegalArgumentException if the hand already contains the card to
+     * be added.
+     */
     public boolean addCard(Card card) {
         if (card == null) {
             return false;
@@ -26,23 +42,39 @@ public class FiveCardPokerHand {
         }
         if (isFull()) {
             return false;
-        }        
-        
+        }
+
         cards.add(card);
         return true;
-    }    
-    
+    }
+
+    /**
+     * Retrieves a copy of the list of cards that the hand contains.
+     *
+     * @return List<Card> A list of the cards in the hand. Never null.
+     */
     public List<Card> getCards() {
         return new ArrayList<Card>(cards);
     }
-    
+
+    /**
+     * Returns the number of cards in the hand.
+     *
+     * @return The number of cards in the hand
+     */
     public int getNumberOfCards() {
         return cards.size();
     }
-    
+
+    /**
+     * Tells if the hand is full.
+     *
+     * The hand is full if it has five cards, and is not full if it has less.
+     * Cards can't be added to a full hand.
+     *
+     * @return true if the hand is full, false otherwise.
+     */
     public boolean isFull() {
         return cards.size() >= 5;
     }
-
-    
 }
--- Poker hand simulator/src/poker/FiveCardPokerHandComparator.java
+++ Poker hand simulator/src/poker/FiveCardPokerHandComparator.java
@@ -11,16 +11,17 @@ public class FiveCardPokerHandComparator implements Comparator<FiveCardPokerHand
     /**
      * Sorts five card poker hands in descending order by hand value.
      * 
-     * Returns a negative number the first hand o1 is better than the 
+     * Returns a negative number if the first hand o1 is better than the 
      * second hand o2, a positive number if o2 is better than o1,
      * and zero if the hands tie.
      * 
      * That means, if we sort a list of FiveCardPokerHand-objects using
      * this comparator, the best hand will be first.
      * 
-     * @param o1
-     * @param o2
-     * @return 
+     * @param o1 FiveCardPokerHand
+     * @param o2 FiveCardPokerHand
+     * @return A negative, positive or zero integer if o1 is better, worse or equal to o2.
+     * @throws IllegalArgumentException if one or both of the hands have less than five cards.
      */
     @Override
     public int compare(FiveCardPokerHand o1, FiveCardPokerHand o2) {
