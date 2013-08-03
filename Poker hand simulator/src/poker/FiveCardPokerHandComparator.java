dc7b20b05f6e776e31da5105b5a6385e630ad867
Sebastian Björkqvist <sebastian.bjorkqvist@helsinki.fi>
2013-08-03 13:37:28 +0300

    Continued writing FiveCardPokerHandComparator

--- Poker hand simulator/src/poker/FiveCardPokerHandComparator.java
+++ Poker hand simulator/src/poker/FiveCardPokerHandComparator.java
@@ -2,6 +2,7 @@ package poker;
 
 import card.Card;
 import card.Rank;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -45,7 +46,15 @@ public final class FiveCardPokerHandComparator implements Comparator<FiveCardPok
         if (!o2.isFull()) {
             return -1;
         }
-        return 0;
+        
+        int hand1Type = determineHandType(o1);
+        int hand2Type = determineHandType(o2);
+        
+        if (hand1Type != hand2Type) {
+            return hand2Type - hand1Type;
+        }
+        
+        return compareHandsOfSameType(o1, o2, hand1Type);        
     }
 
     /**
@@ -54,20 +63,25 @@ public final class FiveCardPokerHandComparator implements Comparator<FiveCardPok
      * Returns an integer representing the type of the hand, according to the
      * list below:
      *
-     * High card - 0 Pair - 1 Two pair - 2 Three of a kind - 3 Straight - 4
-     * Flush - 5 Full house - 6 Four of a kind - 7 Straight flush - 8
+     * High card - 0, Pair - 1, Two pair - 2, Three of a kind - 3, Straight - 4
+     * Flush - 5, Full house - 6, Four of a kind - 7, Straight flush - 8
      *
      * A higher hand type number means a better hand, so with this method we can
      * immediately determine which of two hands is better if they aren't of the
      * same type.
-     * 
-     * The method has modifier protected so that the test class
-     * can access it.
+     *
+     * The method has modifier protected so that the test class can access it.
      *
      * @param hand FiveCardPokerHand
      * @return integer representing the hand type
+     * @throws IllegalArgumentException if the hand doesn't have five cards in
+     * it.
      */
     protected int determineHandType(FiveCardPokerHand hand) {
+        if (hand.getNumberOfCards() != 5) {
+            throw new IllegalArgumentException("The hand must have exactly five cards");
+        }
+
         List<Card> cards = hand.getCards();
 
         Collections.sort(cards);
@@ -85,10 +99,37 @@ public final class FiveCardPokerHandComparator implements Comparator<FiveCardPok
         if (isStraight) {
             return 4;
         }
+        List<Integer> cardsOfSameRankList = cardsOfSameRankList(cards);
 
+        // Four of a kind
+        if (cardsOfSameRankList.get(0) == 4) {
+            return 7;
+        }
+        // Full house and three of a kind
+        if (cardsOfSameRankList.get(0) == 3) {
+            if (cardsOfSameRankList.get(1) == 2) {
+                return 6;
+            }
+            return 3;
+        }
+        // Two pair and pair
+        if (cardsOfSameRankList.get(0) == 2) {
+            if (cardsOfSameRankList.get(1) == 2) {
+                return 2;
+            }
+            return 1;
+        }
+
+        // If none of the above are true, the hand is a high card hand.
         return 0;
     }
 
+    /**
+     * Checks if a hand is a flush.
+     * 
+     * @param cards
+     * @return true if all cards are of the same suit, false otherwise.
+     */
     private boolean isFlush(List<Card> cards) {
         Card first = cards.get(0);
         for (int i = 1; i < cards.size(); i++) {
@@ -99,21 +140,38 @@ public final class FiveCardPokerHandComparator implements Comparator<FiveCardPok
         return true;
     }
 
+    /**
+     * Checks if a hand is a straight.
+     *
+     * The method assumes that the cards are sorted.
+     * 
+     * @param cards
+     * @return True if the hand is a straight, false otherwise.
+     */
     private boolean isStraight(List<Card> cards) {
         if (valueIncreasesByOne(cards, 0)) {
             return true;
         }
-        
+
         /* Since ace has value one when sorting, we need to check the
          * broadway straight A-K-Q-J-T separately
          */
         if (cards.get(0).getRank() == Rank.ACE && cards.get(1).getRank() == Rank.TEN) {
             return valueIncreasesByOne(cards, 1);
         }
-        
+
         return false;
     }
 
+    /**
+     * Checks if the value of the card increases by one starting from an index.
+     * 
+     * The method assumes that the cards are sorted.
+     * 
+     * @param cards List of cards
+     * @param startingIndex starting index
+     * @return true if the value always increases by one, false otherwise.
+     */
     private boolean valueIncreasesByOne(List<Card> cards, int startingIndex) {
         int previousRankValue = cards.get(startingIndex).getRank().getValue();
         for (int i = startingIndex + 1; i < cards.size(); i++) {
@@ -124,5 +182,94 @@ public final class FiveCardPokerHandComparator implements Comparator<FiveCardPok
             previousRankValue = nextRankValue;
         }
         return true;
+    }
+
+    /**
+     * Calculates how many cards of the same rank there is in the hand.
+     *
+     * The sum of the integers in the list is equal to the number of cards in
+     * the hand.
+     *
+     * The method assumes that the cards are sorted.
+     * 
+     * Examples: 
+     * For the hand 9-9-9-K-A the method returns the list {3,1,1} 
+     * For the hand 5-5-4-T-Q the method returns the list {2,1,1,1} 
+     * For the hand 4-4-K-K-K the method returns {3,2} 
+     * For the hand K-T-K-K-K the method returns {4,1} 
+     * For the hand 5-5-7-7-T the method returns {2,2,1} 
+     * For the hand 5-4-8-7-9 the method returns {1,1,1,1,1}
+     *
+     * @param cards The cards of a hand
+     * @return An list of integers, with the highest amount of cards with the
+     * same rank first.
+     */
+    private List<Integer> cardsOfSameRankList(List<Card> cards) {
+        int howManyCardsOfTheSameRank = 1;
+        int previousRankValue = cards.get(0).getRank().getValue();
+        List<Integer> cardsOfSameRankList = new ArrayList<Integer>();
+
+        for (int i = 1; i < cards.size(); i++) {
+            int currentRankValue = cards.get(i).getRank().getValue();
+            if (previousRankValue == currentRankValue) {
+                howManyCardsOfTheSameRank++;
+            } else {
+                cardsOfSameRankList.add(howManyCardsOfTheSameRank);
+                howManyCardsOfTheSameRank = 1;
+                previousRankValue = currentRankValue;
+            }
+        }
+        cardsOfSameRankList.add(howManyCardsOfTheSameRank);
+
+        Collections.sort(cardsOfSameRankList);
+        Collections.reverse(cardsOfSameRankList);
+
+        return cardsOfSameRankList;
+    }
+
+    /**
+     * Compares hands that have the same type.
+     * 
+     * This method assumes that both hands have the type matching
+     * the argument handType, and that both hands are full five card hands.
+     * 
+     * @param o1 Hand 1
+     * @param o2 Hand 2
+     * @param handType hand Type
+     * @return A negative value, positive or zero value if o1 is better, worse or equal to o2.
+     */
+    private int compareHandsOfSameType(FiveCardPokerHand o1, FiveCardPokerHand o2, int handType) {        
+        List<Card> cards1 = o1.getCards();
+        List<Card> cards2 = o2.getCards();
+        Collections.sort(cards1);
+        Collections.sort(cards2);
+        
+        if (handType == 5) {
+            return checkBetterFlush(cards1, cards2);
+        }
+        
+        if (handType == 4 || handType == 8) {
+            
+        }
+        
+        return 0;
+    }
+
+    /**
+     * Checks which of two flushes are better.
+     * @param cards1
+     * @param cards2
+     * @return 
+     */
+    private int checkBetterFlush(List<Card> cards1, List<Card> cards2) {
+        for (int i = 4; i >= 0; i++) {
+            int hand1CardValue = cards1.get(i).getRank().getValue();
+            int hand2CardValue = cards2.get(i).getRank().getValue();
+            int difference = hand2CardValue - hand1CardValue;
+            if (difference != 0) {
+                return difference;
+            }
+        }
+        return 0;
     }
 }
